0eccfacc152f265c03b5ebdf40292331f9bdca41
Flo <amonaholic@gmx.de>
2026-03-06 15:26:31 +0100

    simple calculator and temperature converter developed

--- week1/calculator/src/main/scala/Main.scala
+++ week1/calculator/src/main/scala/Main.scala
@@ -1,5 +1,23 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.StdIn
 
-def msg = "I was compiled by Scala 3. :)"
+@main def hello(): Unit =
+  
+  println("Enter first number: ")
+  val x = StdIn.readLine().toFloat
+
+  println("Enter operator: ")
+  val operator = StdIn.readLine()
+
+  println("Enter second number: ")
+  val y = StdIn.readLine().toFloat
+
+  val result = {
+    operator match
+      case "+" => x + y
+      case "-" => x - y
+      case "*" => x * y
+      case "/" => if y == 0 then println("Can't divide by 0") else x / y
+      case _   => println("Invalid operator")
+  }
+  
+  println(result)
--- week1/temperature-converter/src/main/scala/Main.scala
+++ week1/temperature-converter/src/main/scala/Main.scala
@@ -1,5 +1,21 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.StdIn
 
-def msg = "I was compiled by Scala 3. :)"
+@main def hello(): Unit =
+  
+  print("Enter degrees: ")
+  val degrees = StdIn.readLine().toDouble
+
+  print("Celsius or Fahrenheit: ")
+  val temperatureType = StdIn.readLine().toUpperCase()
+
+  def calculate(degrees: Double, temperatureType: String): Option[Double] =
+      temperatureType match
+        case "C" => Some((degrees * 1.8) + 32)
+        case "F" => Some((degrees - 32) * (5.0 / 9.0))
+        case _ => None
+    
+  val result = calculate(degrees, temperatureType)
+  
+  result match
+    case Some(result) => println(result)
+    case None => println("Invalid temperature type")
